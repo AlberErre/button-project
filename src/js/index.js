776bc12d885949178f74d4b25ef2373da42278aa
Alber Erre <a.ruizblugo@gmail.com>
2018-10-08 19:59:29 +0100

    button working

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,14 @@
 const dice = document.getElementById("dice");
+const button = document.getElementById("button");
+
 
 let randomNumber = () => {
-  
+
   let number = Math.floor((Math.random() * 5 ) + 1);
   dice.innerHTML = number;
 }
 
-// init dice 
-randomNumber();
+button.onclick = () => randomNumber();
 
 
 

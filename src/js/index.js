075fada604e6acaf2829f5d64ec411486d3bb3dc
Alber Erre <a.ruizblugo@gmail.com>
2018-10-08 20:30:24 +0100

    snarkdown added

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,5 @@
+import snarkdown from 'snarkdown';
+
 const dice = document.getElementById("dice");
 const button = document.getElementById("button");
 const records = document.getElementById("records");
@@ -9,8 +11,21 @@ let randomNumber = () => {
   let number = Math.round( (Math.random() * 5 ) + 1);
   PastLaunches.push(number);
   dice.innerHTML = number;
-  records.innerHTML = PastLaunches;
+  records.innerHTML = snarkdown(PastLaunches);
 }
+
+/*
+let showRecords = () => {
+
+    let str = "<table>"
+
+    for (let i = 0; i < PastLaunches.length; i++) {
+        str += "<tr><td colspan=\"2\">" + PastLaunches[i] + "</td></tr>";
+    }
+
+    str += "</table>";
+}
+*/
 
 button.onclick = () => randomNumber();
 
